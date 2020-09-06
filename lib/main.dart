4969ef8a87c17eb0835cdce0174c53c16f1f1c1e
Bash009 <basbashar@gmail.com>
2020-09-06 16:03:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:chatuncle/widgets/new_transaction.dart';
+import 'package:chatuncle/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  String titleInput;
+  String amountInput;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          NewTransaction(),
+          TransactionList(),
+        ],
+      ),
+    );
+  }
+}
